73c50110e017e3e7e87814904ef0fc6d8ecb852b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-06 07:51:11 +0300

    UPD(eslint.config): added new settings and settings.json

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { useState, MouseEvent } from "react";
 import Button from "./components/Button/Button";
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
+	const [counter, setCounter] = useState<number>(0);
 
-  const addCounter = (e: MouseEvent) => {
-    console.log(e.target);
-    setCounter(counter + 1);
-  };
+	const addCounter = (e: MouseEvent) => {
+		console.log(e.target);
+		setCounter(counter + 1);
+	};
 
-  return (
-    <>
-      <Button onClick={addCounter}>{counter ? counter : "Счетичик"}</Button>
-    </>
-  );
+	return (
+		<>
+			<Button onClick={addCounter}>{counter ? counter : "Счетичик"}</Button>
+		</>
+	);
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,14 @@ import type { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
 function Button({ children, className, ...props }: ButtonProps) {
-  return (
-    <button
-      {...props}
-      className={cn(className, styles["button"], styles["accent"])}
-    >
-      {children}
-    </button>
-  );
+	return (
+		<button
+			{...props}
+			className={cn(className, styles["button"], styles["accent"])}
+		>
+			{children}
+		</button>
+	);
 }
 
 export default Button;
