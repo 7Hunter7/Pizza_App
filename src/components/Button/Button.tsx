631b285c65654751de4c5468389a72d3f2c71a76
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-08 10:10:25 +0300

    UPD(Button): added type prop 'appearence' and using it to small and big styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,15 @@ import styles from './Button.module.scss';
 import type { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
-function Button({ children, className, ...props }: ButtonProps) {
+function Button({ children, className, appearence = 'small', ...props }: ButtonProps) {
 	return (
 		<button
 			{...props}
-			className={cn(className, styles['button'], styles['accent'])}
+			className={cn(className, styles['button'], styles['accent'], {
+				[styles['small']]: appearence === 'small',
+				[styles['big']]: appearence === 'big',
+			}
+			)}
 		>
 			{children}
 		</button>
