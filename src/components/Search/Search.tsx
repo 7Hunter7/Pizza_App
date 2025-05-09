2008c8a16265ee63dc27597d1eba5e98998ab328
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-09 22:37:17 +0300

    UPD(Search): added svg search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,9 +5,18 @@ import type { SearchProps } from './Search.props';
 
 const Search = forwardRef<HTMLInputElement, SearchProps>(function Search({ className, isValid = true, ...props }, ref) {
 	return (
-		<input ref={ref} className={cn(className, styles['search'], {
-			[styles['invalid']]: !isValid,
-		})} {...props}/>
+		<div className={styles['search-wrapper']}>
+			<input ref={ref} className={cn(className, styles['search'], {
+				[styles['invalid']]: !isValid,
+			})} {...props}/>
+			<svg className={styles['search-icon']}				
+				xmlns="http://www.w3.org/2000/svg" 
+				width="32" 
+				height="32" 
+				viewBox="0 0 24 24">
+				<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.5 17.5L22 22m-2-11a9 9 0 1 0-18 0a9 9 0 0 0 18 0" color="currentColor"/>
+			</svg>			
+		</div>
 	);
 });
 
