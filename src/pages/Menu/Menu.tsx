6a295aad3050643149f37694436d012e8c08c943
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-11 12:31:55 +0300

    UPD(Menu): added using useState() and try/catch for getMenu()

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
@@ -6,11 +7,19 @@ import type { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu() {
+	const [products, setProducts ] = useState<Product[]>([]);
+
 	const getMenu = async() => {
+		try {
 		const res = await fetch(`${PREFIX}/products`);
 		if (!res.ok) return;
 		const data = await res.json() as Product[];
-		console.log('data: ', data);
+		console.log('Data: ', data);
+		setProducts(data);
+		} catch(err) {
+			console.error('Error: ', err);
+		}
+	};
 
 	return (
 		<>
