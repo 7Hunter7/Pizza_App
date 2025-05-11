2fe3a9cfdeca0376b52b7968c50624f0ee102c9e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-11 17:10:24 +0300

    UPD(Login): updated template using Headling, Input and Button components, and Link

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,26 @@
-import styles from './Login.module.css';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Headling from '../../components/Headling/Headling';
+import Input from '../../components/Input/Input';
+import styles from './Login.module.scss';
 
 export function Login() {
-	return <>Login</>
+	return <div className={styles.login}>
+		<Headling>Вход</Headling>
+		<form className={styles.form}>
+			<div className={styles.field}>
+				<label htmlFor='email'>Ваш email</label>
+				<Input id='email' placeholder='Email'/>
+			</div>
+			<div className={styles.field}>
+				<label htmlFor='password'>Ваш пароль</label>
+				<Input id='password' placeholder='Пароль'/>
+			</div>
+			<Button appearence='big'>Вход</Button>
+		</form>
+			<div>
+				<p>Нет аккаунта?</p>
+				<Link to='/auth/register'>Зарегистрироваться</Link>
+			</div>
+	</div>
 };
