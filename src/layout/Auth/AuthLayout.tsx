52f5b1977fb7b45523454fff95884fea6e6bf3d2
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-11 16:44:57 +0300

    UPD(AuthLayout): added styles for layout, logo and content

--- src/layout/Auth/AuthLayout.tsx
+++ src/layout/Auth/AuthLayout.tsx
@@ -6,6 +6,7 @@ export function AuthLayout() {
 
   return <div className={styles.layout}>
     <div className={styles.logo}>
+      <img src="/pizza-logo.jpg" alt="Логотип компании" />
     </div>
     <div className={styles.content}>
       <Outlet/> 
