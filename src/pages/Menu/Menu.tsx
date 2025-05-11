c5b9fcbb6f7fa57185e5721403c4a6865a1023c2
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-11 12:27:16 +0300

    UPD(Menu): added getMenu() using: fetch('PREFIX') and interface Product[]

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,17 @@
 import Headling from '../../components/Headling/Headling';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import Search from '../../components/Search/Search';
+import { PREFIX } from '../../helpers/API';
+import type { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
 
 export function Menu() {
+	const getMenu = async() => {
+		const res = await fetch(`${PREFIX}/products`);
+		if (!res.ok) return;
+		const data = await res.json() as Product[];
+		console.log('data: ', data);
+
 	return (
 		<>
 			<div className={styles.head}>
