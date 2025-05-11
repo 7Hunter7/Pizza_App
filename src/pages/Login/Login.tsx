d59e36c453a2963f7644ffc39ffab532731a4567
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-11 17:23:23 +0300

    UPD(Login): added submit() using FormEvent

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,18 +3,24 @@ import Button from '../../components/Button/Button';
 import Headling from '../../components/Headling/Headling';
 import Input from '../../components/Input/Input';
 import styles from './Login.module.scss';
+import type { FormEvent } from 'react';
 
 export function Login() {
+	const submit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
 	return <div className={styles.login}>
 		<Headling>Вход</Headling>
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={submit}>
 			<div className={styles.field}>
 				<label htmlFor='email'>Ваш email</label>
-				<Input id='email' placeholder='Email'/>
+				<Input id='email' name='email' placeholder='Email'/>
 			</div>
 			<div className={styles.field}>
 				<label htmlFor='password'>Ваш пароль</label>
-				<Input id='password' placeholder='Пароль'/>
+				<Input id='password' name='password' placeholder='Пароль'/>
 			</div>
 			<Button appearence='big'>Вход</Button>
 		</form>
