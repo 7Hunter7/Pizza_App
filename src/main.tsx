e2330656dc4879d15871c0f34e3ecac456c317cd
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-13 08:00:36 +0300

    UPD(main.tsx): added using Provider with store

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout.tsx';
 import { Login } from './pages/Login/Login.tsx';
 import { Register } from './pages/Register/Register.tsx';
 import { RequireAuth } from './helpers/RequireAuth.tsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -64,6 +66,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
+		<Provider store={store}>
 			<RouterProvider router={router}/>
+		</Provider>
 	</StrictMode>
 );
