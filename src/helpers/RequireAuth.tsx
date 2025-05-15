4c57f4f2f8b3d562e1340f287ef07f8f4a042990
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-15 07:32:17 +0300

    UPD(RequireAuth): updated jwt using hook useSelector() with using type RootStore in RequireAuth()

--- src/helpers/RequireAuth.tsx
+++ src/helpers/RequireAuth.tsx
@@ -1,8 +1,10 @@
 import type { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import type { RootStore } from '../store/store';
 
 export const RequireAuth = ({children}: {children: ReactNode}) => {
-  const jwt = localStorage.getItem('jwt');
+  const jwt = useSelector((s: RootStore) => s.user.jwt);
   if (!jwt) {
     return <Navigate to='/auth/login' replace/>
     }
