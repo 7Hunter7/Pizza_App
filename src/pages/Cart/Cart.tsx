08d9c951bad4fc2b0b95f616310c10aaf8d83e1c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-20 07:25:19 +0300

    UPD(Cart): added using <Headling> and useSelector() for get items

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux';
+import Headling from '../../components/Headling/Headling';
 import styles from './Cart.module.css';
+import type { RootStore } from '../../store/store';
 
 export function Cart() {
+  const items = useSelector((s: RootStore) => s.cart.items);
+
 	return (
-    <>
-      Cart
-    </>
+    <div className={styles.cart}>
+      <Headling>Корзина</Headling>
+    </div>
 	);
 };
