a759a9dd02b4f81b782449c10244a55f37b1f79b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-20 18:46:51 +0300

    UPD(Cart): added styles for headling

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import Headling from '../../components/Headling/Headling';
-import styles from './Cart.module.css';
+import styles from './Cart.module.scss';
 import type { RootStore } from '../../store/store';
 import CartItem from '../../components/CartItem/CartItem';
 import { useEffect, useState } from 'react';
@@ -29,7 +29,7 @@ export function Cart() {
 
 	return (
     <div className={styles.cart}>
-      <Headling>Корзина</Headling>
+      <Headling className={styles.headling}>Корзина</Headling>
       {items.map(i => {
         const product = cartProducts.find(p => p.id === i.id);
         if(!product) return;
