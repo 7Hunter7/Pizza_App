0702a6b5228888636ba5fa0fbb1c572d3dbcab94
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-21 21:30:10 +0300

    UPD: added new page Success

---
+++ src/pages/Success/Success.tsx
@@ -0,0 +1,15 @@
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import styles from './Success.module.scss';
+
+export function Success() {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.success}>
+      <img src="" alt="Изображение пиццы" />
+      <div className={styles.text}>Ваш заказ успешно оформлен!</div>
+      <Button appearence='big' onClick={() => navigate('/')}>Сделать новый</Button>
+    </div>
+  )
+}
